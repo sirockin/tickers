881bb17445cf96eee58313bcd30ffaccbcfa93bd
Dave Sirockin <dave@sirockin.com>
2025-10-05 11:44:47 +0200

    feat: add jitter

--- exponential.go
+++ exponential.go
@@ -1,44 +1,66 @@
 package tickers
 
 import (
-    "time"
+	"math/rand/v2"
+	"time"
 )
 
 type Exponential struct {
-    C        chan time.Time
-    done     chan struct{}
-    interval time.Duration
-    factor   float64
+	C        chan time.Time
+	done     chan struct{}
+	interval time.Duration
+	factor   float64
+	jitter   time.Duration
 }
 
-func NewExponential(initialDuration time.Duration, factor float64) *Exponential {
-    e := &Exponential{
-        C:        make(chan time.Time),
-        done:     make(chan struct{}),
-        interval: initialDuration,
-        factor:   factor,
-    }
+type ExponentialOption func(*Exponential)
 
-    go func() {
-        ticker := time.NewTimer(e.interval)
-        defer ticker.Stop()
+func WithJitter(jitter time.Duration) ExponentialOption {
+	return func(e *Exponential) {
+		e.jitter = jitter
+	}
+}
 
-        for {
-            select {
-            case t := <-ticker.C:
-                e.C <- t
-                ticker.Stop()
-                e.interval = time.Duration(float64(e.interval) * e.factor)
-                ticker = time.NewTimer(e.interval)
-            case <-e.done:
-                return
-            }
-        }
-    }()
+func NewExponential(initialDuration time.Duration, factor float64, opts ...ExponentialOption) *Exponential {
+	e := &Exponential{
+		C:        make(chan time.Time),
+		done:     make(chan struct{}),
+		interval: initialDuration,
+		factor:   factor,
+	}
 
-    return e
+	for _, opt := range opts {
+		opt(e)
+	}
+
+	go func() {
+		nextInterval := e.interval
+		if e.jitter > 0 {
+			nextInterval += time.Duration(rand.Int64N(int64(e.jitter)))
+		}
+		ticker := time.NewTimer(nextInterval)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case t := <-ticker.C:
+				e.C <- t
+				ticker.Stop()
+				e.interval = time.Duration(float64(e.interval) * e.factor)
+				nextInterval = e.interval
+				if e.jitter > 0 {
+					nextInterval += time.Duration(rand.Int64N(int64(e.jitter)))
+				}
+				ticker = time.NewTimer(nextInterval)
+			case <-e.done:
+				return
+			}
+		}
+	}()
+
+	return e
 }
 
 func (e *Exponential) Stop() {
-    close(e.done)
+	close(e.done)
 }
--- exponential_test.go
+++ exponential_test.go
@@ -67,6 +67,65 @@ func TestExponentialIntervals(t *testing.T) {
 	}
 }
 
+func TestExponentialWithJitter(t *testing.T) {
+	cases := map[string]struct {
+		initialDuration   time.Duration
+		factor            float64
+		jitter            time.Duration
+		expectedIntervals []time.Duration
+	}{
+		"factor 2": {
+			initialDuration: 1 * time.Second,
+			factor:          2,
+			jitter:          500 * time.Millisecond,
+			expectedIntervals: []time.Duration{
+				1 * time.Second,
+				2 * time.Second,
+				4 * time.Second,
+				8 * time.Second,
+			},
+		},
+		"factor 3": {
+			initialDuration: 500 * time.Millisecond,
+			factor:          3,
+			jitter:          250 * time.Millisecond,
+			expectedIntervals: []time.Duration{
+				500 * time.Millisecond,
+				1500 * time.Millisecond,
+				4500 * time.Millisecond,
+				13500 * time.Millisecond,
+			},
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			// Run inside synctest to use mocked time
+			synctest.Test(t, func(t *testing.T) {
+				ticker := tickers.NewExponential(tc.initialDuration, tc.factor, tickers.WithJitter(tc.jitter))
+				defer ticker.Stop()
+				for i, expectedInterval := range tc.expectedIntervals {
+					time.Sleep(expectedInterval - 1*time.Millisecond)
+					synctest.Wait() // Make sure any goroutines are unlocked
+					if _, ok := receivedValue(ticker.C); ok {
+						t.Fatalf("Ticker channel should not have value before interval %d", i)
+					}
+					earliest := time.Now()
+					time.Sleep(1 * time.Millisecond+tc.jitter)
+					synctest.Wait() // Make sure any goroutines are unlocked
+					got, ok := receivedValue(ticker.C)
+					if !ok {
+						t.Fatalf("Ticker channel should have value after interval has elapsed")
+					}
+					latest := time.Now()
+					if got.After(latest) || got.Before(earliest) {
+						t.Fatalf("Expected: time between %v and %v but got %v", earliest, latest,  got)
+					}
+				}
+			})
+		})
+	}
+}
+
 func TestExponentialStop(t *testing.T) {
 	synctest.Test(t, func(t *testing.T) {
 		ticker := tickers.NewExponential(1*time.Second, 2)
